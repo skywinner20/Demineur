75d7a35abe91137361b4975e45b831c4bf68d820
skywinner20 <94059458+skywinner20@users.noreply.github.com>
2022-01-11 10:33:56 +0100

    Update demineur.py

--- demineur.py
+++ demineur.py
@@ -464,7 +464,6 @@ while flag_jeu == False: #permet de rejouer
     # - - -
     if flag_turtle == True:
         tr.discworld.reset()
-        tr.turtle.reset()
 
     # - - - Replay - - -
     replay = False
